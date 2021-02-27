a76317952de2beaaecb0ef472053fbd2e5eb68dd
Volodymyr Osadchuk <voosa@smart-it.com>
2021-02-27 09:42:51 +0200

    init

---
+++ PhotosApp/Controllers/PhotosController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace PhotosApp.Controllers
+{
+    [ApiController]
+    public class PhotosController : ControllerBase
+    {
+        private readonly ILogger<PhotosController> _logger;
+
+        public PhotosController(ILogger<PhotosController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("/search/{searchTerm}")]
+        public IActionResult Get(string searchTerm)
+        {
+            return null;
+        }
+    }
+}
